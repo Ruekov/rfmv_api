9106ca8b7e8210e9483bf39e6d79cdca86c73715
Guillem <guillemrueda@gmail.com>
2021-05-20 22:19:55 +0200

    consultes segons temes

--- api/games/index.php
+++ api/games/index.php
@@ -12,6 +12,7 @@
     $db = $database->getConnection();
     $id = false;
     $url = false;
+    $genere = false;   
 
     if(isset($_GET['id'])){
         $id = $_GET['id'];
@@ -165,6 +166,207 @@
             );
 
             $gameArr["game"] = $e;
+        }
+        echo json_encode($gameArr);
+
+    }
+    else if(isset($_GET['genere'])){
+        $genere = $_GET['genere'];
+        $genereProcessed = str_replace("-", " ", $genere);
+
+        $items = new Games($db);
+
+        $stmt = $items->getGamesByGenere($genereProcessed);
+
+        $gameArr = array();
+        $gameArr["games"] = array();
+
+
+
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+            extract($row);
+            $e = array(
+                "GameID" => $GameID,
+                "Name" => $Name,
+                "Published" => $Published,
+                "url" => $url
+            );
+
+            array_push($gameArr["games"], $e);
+        }
+        echo json_encode($gameArr);
+
+    }
+    else if(isset($_GET['language'])){
+        $language = $_GET['language'];
+
+        $items = new Games($db);
+
+        $stmt = $items->getGamesByLanguage($language);
+
+        $gameArr = array();
+        $gameArr["games"] = array();
+
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+            extract($row);
+            $e = array(
+                "GameID" => $GameID,
+                "Name" => $Name,
+                "Published" => $Published,
+                "url" => $url
+            );
+
+            array_push($gameArr["games"], $e);
+        }
+        echo json_encode($gameArr);
+
+    }
+    else if(isset($_GET['platform'])){
+        $platform = $_GET['platform'];
+        $platformProcessed = str_replace("-", " ", $platform);
+
+        $items = new Games($db);
+
+        $stmt = $items->getGamesByPlatform($platformProcessed);
+
+        $gameArr = array();
+        $gameArr["games"] = array();
+
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+            extract($row);
+            $e = array(
+                "GameID" => $GameID,
+                "Name" => $Name,
+                "Published" => $Published,
+                "url" => $url
+            );
+
+            array_push($gameArr["games"], $e);
+        }
+        echo json_encode($gameArr);
+
+    }
+    else if(isset($_GET['developer'])){
+        $developer = $_GET['developer'];
+        $developerProcessed = str_replace("-", " ", $developer);
+
+        $items = new Games($db);
+
+        $stmt = $items->getGamesByDeveloper($developerProcessed);
+
+        $gameArr = array();
+        $gameArr["games"] = array();
+
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+            extract($row);
+            $e = array(
+                "GameID" => $GameID,
+                "Name" => $Name,
+                "Published" => $Published,
+                "url" => $url
+            );
+
+            array_push($gameArr["games"], $e);
+        }
+        echo json_encode($gameArr);
+
+    }
+    else if(isset($_GET['publisher'])){
+        $publisher = $_GET['publisher'];
+        $publisherProcessed = str_replace("-", " ", $publisher);
+
+        $items = new Games($db);
+
+        $stmt = $items->getGamesByPublisher($publisherProcessed);
+
+        $gameArr = array();
+        $gameArr["games"] = array();
+
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+            extract($row);
+            $e = array(
+                "GameID" => $GameID,
+                "Name" => $Name,
+                "Published" => $Published,
+                "url" => $url
+            );
+
+            array_push($gameArr["games"], $e);
+        }
+        echo json_encode($gameArr);
+
+    }
+    else if(isset($_GET['theme'])){
+        $theme = $_GET['theme'];
+        $themeProcessed = str_replace("-", " ", $theme);
+
+        $items = new Games($db);
+
+        $stmt = $items->getGamesByTheme($themeProcessed);
+
+        $gameArr = array();
+        $gameArr["games"] = array();
+
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+            extract($row);
+            $e = array(
+                "GameID" => $GameID,
+                "Name" => $Name,
+                "Published" => $Published,
+                "url" => $url
+            );
+
+            array_push($gameArr["games"], $e);
+        }
+        echo json_encode($gameArr);
+
+    }
+    else if(isset($_GET['style'])){
+        $style = $_GET['style'];
+        $styleProcessed = str_replace("-", " ", $style);
+
+        $items = new Games($db);
+
+        $stmt = $items->getGamesByStyle($styleProcessed);
+
+        $gameArr = array();
+        $gameArr["games"] = array();
+
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+            extract($row);
+            $e = array(
+                "GameID" => $GameID,
+                "Name" => $Name,
+                "Published" => $Published,
+                "url" => $url
+            );
+
+            array_push($gameArr["games"], $e);
+        }
+        echo json_encode($gameArr);
+
+    }
+    else if(isset($_GET['country'])){
+        $country = $_GET['country'];
+        $countryProcessed = str_replace("-", " ", $country);
+
+        $items = new Games($db);
+
+        $stmt = $items->getGamesByCountry($countryProcessed);
+
+        $gameArr = array();
+        $gameArr["games"] = array();
+
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+            extract($row);
+            $e = array(
+                "GameID" => $GameID,
+                "Name" => $Name,
+                "Published" => $Published,
+                "url" => $url
+            );
+
+            array_push($gameArr["games"], $e);
         }
         echo json_encode($gameArr);
 
--- class/games.php
+++ class/games.php
@@ -43,6 +43,160 @@
             return $stmt;
         }
 
+        public function getGamesByGenere($genereGame){
+            $sqlQuery = "SELECT 
+            g.GameId AS 'GameID', 
+            g.publishedName AS 'Name',
+            g.url AS 'url',
+            gp.ReleaseDate As 'Published'
+            FROM        
+            " . $this->db_table . " g
+            LEFT JOIN GamePlatforms gp ON gp.GameId = g.GameId
+            LEFT JOIN GameGeneres ggn ON ggn.GameId = g.GameId
+            LEFT JOIN Generes gn ON ggn.GenereID = gn.GenereID
+            WHERE gn.Name= '" . $genereGame . "'
+			GROUP BY g.gameId  
+            ";
+            $stmt = $this->conn->prepare($sqlQuery);
+            $stmt->execute();
+            return $stmt;
+        }
+
+        public function getGamesByLanguage($language){
+            $sqlQuery = "SELECT 
+            g.GameId AS 'GameID', 
+            g.publishedName AS 'Name',
+            g.url AS 'url',
+            gp.ReleaseDate As 'Published'
+            FROM        
+            " . $this->db_table . " g
+            LEFT JOIN GamePlatforms gp ON gp.GameId = g.GameId
+            LEFT JOIN GameLanguages glan ON glan.GameId = g.GameId
+            LEFT JOIN Idioms id ON glan.IdiomID = id.IdiomID
+            WHERE id.Name= '" . $language . "'
+			GROUP BY g.gameId  
+            ";
+            $stmt = $this->conn->prepare($sqlQuery);
+            $stmt->execute();
+            return $stmt;
+        }
+
+        public function getGamesByPlatform($platform){
+            $sqlQuery = "SELECT 
+            g.GameId AS 'GameID', 
+            g.publishedName AS 'Name',
+            g.url AS 'url',
+            gp.ReleaseDate As 'Published'
+            FROM        
+            " . $this->db_table . " g
+            LEFT JOIN GamePlatforms gp ON gp.GameId = g.GameId
+            LEFT JOIN Platforms pl ON pl.PlatformID = gp.PlatformID
+            WHERE pl.Name= '" . $platform . "'
+			GROUP BY g.gameId  
+            ";
+            $stmt = $this->conn->prepare($sqlQuery);
+            $stmt->execute();
+            return $stmt;
+        }
+
+        public function getGamesByDeveloper($developer){
+            $sqlQuery = "SELECT 
+            g.GameId AS 'GameID', 
+            g.publishedName AS 'Name',
+            g.url AS 'url',
+            gp.ReleaseDate As 'Published'
+            FROM        
+            " . $this->db_table . " g
+            LEFT JOIN GamePlatforms gp ON gp.GameId = g.GameId
+            LEFT JOIN Platforms pl ON pl.PlatformID = gp.PlatformID
+            LEFT JOIN GameDevelopers gDev ON gDev.GameId = g.GameId
+            LEFT JOIN Companies com ON com.CompanyID = gDev.CompanyID
+            WHERE com.Name= '" . $developer . "'
+			GROUP BY g.gameId  
+            ";
+            $stmt = $this->conn->prepare($sqlQuery);
+            $stmt->execute();
+            return $stmt;
+        }
+
+        public function getGamesByPublisher($publisher){
+            $sqlQuery = "SELECT 
+            g.GameId AS 'GameID', 
+            g.publishedName AS 'Name',
+            g.url AS 'url',
+            gp.ReleaseDate As 'Published'
+            FROM        
+            " . $this->db_table . " g
+            LEFT JOIN GamePlatforms gp ON gp.GameId = g.GameId
+            LEFT JOIN Platforms pl ON pl.PlatformID = gp.PlatformID
+            LEFT JOIN GamePublishers gPub ON gPub.GameId = g.GameId
+            LEFT JOIN Companies com ON com.CompanyID = gPub.CompanyID
+            WHERE com.Name= '" . $publisher . "'
+			GROUP BY g.gameId  
+            ";
+            $stmt = $this->conn->prepare($sqlQuery);
+            $stmt->execute();
+            return $stmt;
+        }
+
+        public function getGamesByTheme($theme){
+            $sqlQuery = "SELECT 
+            g.GameId AS 'GameID', 
+            g.publishedName AS 'Name',
+            g.url AS 'url',
+            gp.ReleaseDate As 'Published'
+            FROM        
+            " . $this->db_table . " g
+            LEFT JOIN GamePlatforms gp ON gp.GameId = g.GameId
+            LEFT JOIN Platforms pl ON pl.PlatformID = gp.PlatformID
+            LEFT JOIN GameThemes gt ON gt.GameId = g.GameId
+            LEFT JOIN Themes th ON th.ThemeID = gt.ThemeID
+            WHERE th.Name= '" . $theme . "'
+			GROUP BY g.gameId  
+            ";
+            $stmt = $this->conn->prepare($sqlQuery);
+            $stmt->execute();
+            return $stmt;
+        }
+
+        public function getGamesByStyle($style){
+            $sqlQuery = "SELECT 
+            g.GameId AS 'GameID', 
+            g.publishedName AS 'Name',
+            g.url AS 'url',
+            gp.ReleaseDate As 'Published'
+            FROM        
+            " . $this->db_table . " g
+            LEFT JOIN GamePlatforms gp ON gp.GameId = g.GameId
+            LEFT JOIN GameStyles gst ON gst.GameId = g.GameId
+            LEFT JOIN Styles st ON gst.ThemeID = st.ThemeID
+            WHERE st.Name= '" . $style . "'
+			GROUP BY g.gameId  
+            ";
+            $stmt = $this->conn->prepare($sqlQuery);
+            $stmt->execute();
+            return $stmt;
+        }
+
+        public function getGamesByCountry($country){
+            $sqlQuery = "SELECT 
+            g.GameId AS 'GameID', 
+            g.publishedName AS 'Name',
+            g.url AS 'url',
+            gp.ReleaseDate As 'Published'
+            FROM        
+            " . $this->db_table . " g
+            LEFT JOIN GamePlatforms gp ON gp.GameId = g.GameId
+            LEFT JOIN GameCountries gc ON gc.GameId = g.GameId
+            LEFT JOIN Countries ct ON gc.CountryID = ct.CountryID
+            WHERE ct.Name= '" . $country . "'
+			GROUP BY g.gameId  
+            ";
+            $stmt = $this->conn->prepare($sqlQuery);
+            $stmt->execute();
+            return $stmt;
+        }
+
     }
 ?>
 
